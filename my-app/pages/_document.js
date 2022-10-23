6849383e92b7183b7cb906c559b1e6cb0e99281d
shusuke919 <sh091965@gmail.com>
2022-10-23 17:08:56 +0900

    環境構築完了

---
+++ my-app/pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fon  ts.googleapis.com/css2?family=Coming+Soon&family=Outfit:wght@100;200;300;400;500;600;700;800&family=Source+Sans+Pro&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
