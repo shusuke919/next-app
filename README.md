c03bec63ea60b50c75af41b0954552d0e93577f0
shusuke919 <sh091965@gmail.com>
2022-10-24 16:27:04 +0900

    add  navbar

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # next-app
+
+next.js、react、tailwindを使ったwebsite作成の写経
+
+https://www.youtube.com/watch?v=HVyct9EUNP8&t=1405s
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../components/Hero'
 
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         
       </Head>
-
+       <Hero heading='captur Photography' message='I caputure moments in nature and keep them alive.' />
      
     </div>
   )
